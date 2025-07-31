0d310913562210d221d0843214b5b6e5f3d2f363
Geovanny <birdcode.py@gmail.com>
2025-07-31 13:33:17 -0500

    Login page development part 2 - Creation of header and tabs for the login section

--- login.py
+++ login.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write('Hello World')
+st.header('Login procedures')
+login, passcreator = st.tabs(['Log in', 'Password creator'])
+
+with login:
+
+	st.write('')
+	st.write('Log in to BirdCipher Machine!')
+	
