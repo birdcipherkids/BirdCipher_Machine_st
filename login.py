b9236e8521d3b035c6ab2baa57f0d38ec0261714
Geovanny <birdcode.py@gmail.com>
2025-07-31 14:42:04 -0500

    First image insertion - BirdCipher

--- login.py
+++ login.py
@@ -7,9 +7,13 @@ with login:
 
 	with st.form(key = 'login_button_form', enter_to_submit = False):
 
-		st.subheader('Log in to BirdCipher Machine!')
-		st.text_input('Username', key = 'username', width = 400)
-		st.text_input('Password', key = 'main_password', width = 400)
-		st.text_input('Role', key = 'role', width = 400)
-		st.form_submit_button('Send data')
+		col1, col2 = st.columns([3, 1])
+
+		col1.subheader('Log in to BirdCipher Machine!')
+		col1.text_input('Username', key = 'username', width = 500)
+		col1.text_input('Password', key = 'main_password', width = 500)
+		col1.text_input('Role', key = 'role', width = 500)
+		col1.form_submit_button('Send data')
+
+		col2.image('Images/BirdCipher-logo.png')
 
