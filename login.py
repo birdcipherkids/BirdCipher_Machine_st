6dbc12376722ace9937ec40a5766b1ff5d8e9e56
Geovanny <birdcode.py@gmail.com>
2025-07-31 14:20:09 -0500

    Login page development - Creation of st.form, text inputs for the login credentials and the dend data assigment to the submit button

--- login.py
+++ login.py
@@ -5,6 +5,11 @@ login, passcreator = st.tabs(['Log in', 'Password creator'])
 
 with login:
 
-	st.write('')
-	st.write('Log in to BirdCipher Machine!')
-	
+	with st.form(key = 'login_button_form', enter_to_submit = False):
+
+		st.subheader('Log in to BirdCipher Machine!')
+		st.text_input('Username', key = 'username', width = 400)
+		st.text_input('Password', key = 'main_password', width = 400)
+		st.text_input('Role', key = 'role', width = 400)
+		st.form_submit_button('Send data')
+
